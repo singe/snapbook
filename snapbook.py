5391fb13101bdbbda64e2d60536ba1cd77848d4c
singe <singe-github@singe.za.net>
2016-02-01 09:43:28 +0200

    Added some more exception handling, and comments

--- snapbook.py
+++ snapbook.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.action_chains import ActionChains
 import unittest, time, re
@@ -40,56 +41,74 @@ class FbLogin(unittest.TestCase):
         #driver.find_element_by_id("u_0_2").click()
         #driver.find_element_by_id("checkpointSubmitButton").click()
 
+        #Navigate to all activity page
         driver.get(self.base_url + "PUT YOUR FB USERNAME HERE (NOT YOUR EMAIL ADDRESS)/allactivity")
         posts = []
-        index = -1 
+        index = -1 #If you're recovering from a crash, change this to the last index you saw
         while True:
             count = 0
+            #Find all the privacy blocks
             for divs in driver.find_elements_by_xpath("//div[@class='uiSelector inlineBlock audienceSelector audienceSelectorNoTruncate dynamicIconSelector uiSelectorRight uiSelectorNormal uiSelectorDynamicTooltip']"):
+                #Skip ones we've seen before
                 if count <= index:
                     print('continue '+str(count)+' index '+str(index))
                     count = count + 1
                     continue
                 #embed()
                 try:
+                    #Move to the privacy button to avoid "Element Not Visible" errors
                     actions = ActionChains(driver)
                     actions.move_to_element(divs)
                     actions.perform()
                     print('move to div '+str(count)+' index '+str(index))
+                    #Find the link to click
                     foo=divs.find_element_by_xpath(".//a[@class='uiSelectorButton uiButton uiButtonSuppressed uiButtonNoText']")
                 except NoSuchElementException:
+                    #Sometimes we get out of state and need to reset things
                     driver.find_element_by_tag_name('body').click()
                     actions = ActionChains(driver)
                     actions.move_to_element(divs)
                     actions.perform()
                     foo=divs.find_element_by_xpath(".//a[@class='uiSelectorButton uiButton uiButtonSuppressed uiButtonNoText']")
                 try:
+                    #Try add the element to our index, if we haven't seen it end up at the exception
                     posts.index(foo)
                 except ValueError:
+                    #If we're here, it's a new element, add it
                     posts.append(foo) 
                     index = posts.index(foo)
                     print('add to index '+str(count)+' index '+str(index))
+                    #Try move to the specific link just in case
                     actions = ActionChains(driver)
                     actions.move_to_element(foo)
                     actions.perform()
-                    #foo.send_keys(Keys.ENTER)
+                    #Try and avoid performing the action on stuff already hidden
+                    #This tooltip is only set correctly some of the time
                     if foo.get_attribute('aria-label') != 'Only Me':
+                        #The move_to_element doesn't seem to work here, and a click will throw a not visible exception, so use a keypress instead, has the benefit of scrolling as well
                         foo.send_keys(Keys.ENTER)
+                        #I had a fancy loop, it was finnicky, this works well on an average connection
                         time.sleep(0.5)
+                        #Again clicks and move_to don't work in the popup
                         divs.find_element_by_xpath(".//li[@data-label='Only Me']/a").send_keys(Keys.ENTER)
-                        #foo.send_keys(Keys.SPACE)
-                        #time.sleep(0.5)
-                        #divs.find_element_by_xpath(".//li[@data-label='Only Me']/a").click()
                 count = count + 1
-
-            baz=driver.find_element_by_xpath("//a[@title='Make your next career move to our brilliant company.']")
-            actions = ActionChains(driver)
-            actions.move_to_element(baz)
-            actions.perform()
-            baz.send_keys(Keys.SPACE)
-            baz=driver.find_element_by_xpath("//span[@class='uiMorePagerLoader pam uiBoxLightblue']")
-            baz.send_keys(Keys.SPACE)
-            time.sleep(2)
+            #Move to the bottom of the page to load more posts
+            try:
+                #First move to the bottom to trigger the loading image
+                baz=driver.find_element_by_xpath("//a[@title='Make your next career move to our brilliant company.']")
+                actions = ActionChains(driver)
+                actions.move_to_element(baz)
+                actions.perform()
+                #Sometimes move_to doesn't go far enough, so do a space for good luck
+                baz.send_keys(Keys.SPACE)
+                #Try move to the loading element
+                baz=driver.find_element_by_xpath("//span[@class='uiMorePagerLoader pam uiBoxLightblue']")
+                baz.send_keys(Keys.SPACE)
+                #Wait for the new posts to load, you can shorten this, but then you sometimes end up iterating through the whole list again for no reason, this is a reasonable trade off
+                time.sleep(2)
+            except StaleElementReferenceException:
+                #When the page gets long enough, the browser seems to forget some elements
+                pass
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
